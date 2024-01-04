b88dfbd1161b122e2d35d32d2532822ecc089b52
wangkaili0110 <129839207+wangkaili0110@users.noreply.github.com>
2024-01-04 09:15:57 +0800

    Delete model/include directory

--- model/include/matmult.h
+++
@@ -1,7 +0,0 @@
-#include <torch/types.h>
-
-
-void launch_matmult(const at::Tensor query,
-                    const at::Tensor key,
-                    at::Tensor output,
-                    int bs, int nh, int ql, int kl, int ic);
