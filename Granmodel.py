b01cf08699dbbabf298c8f20dc8650f4e34d53a5
wangkaili0110 <129839207+wangkaili0110@users.noreply.github.com>
2024-09-06 11:04:54 +0800

    Update Granmodel.py

--- Granmodel.py
+++ Granmodel.py
@@ -78,7 +78,7 @@ class GRAN(nn.Module):
         self.gather_local_0 = Local_op(in_channels=128, out_channels=128)
         self.gather_local_1 = Local_op(in_channels=256, out_channels=256)
 
-        self.pt_last = Rough_Set_Transformer(args)
+        self.pt_last = Granular_Transformer(args)
 
         self.conv_fuse = nn.Sequential(nn.Conv1d(1280, 1024, kernel_size=1, bias=False),
                                     nn.BatchNorm1d(1024),
@@ -119,9 +119,9 @@ class GRAN(nn.Module):
 
         return x
 
-class Rough_Set_Transformer(nn.Module):
+class Granular_Transformer(nn.Module):
     def __init__(self, args, channels=256):
-        super(Rough_Set_Transformer, self).__init__()
+        super(Granular_Transformer, self).__init__()
         self.args = args
         self.conv1 = nn.Conv1d(channels, channels, kernel_size=1, bias=False)
         self.conv2 = nn.Conv1d(channels, channels, kernel_size=1, bias=False)
@@ -129,10 +129,10 @@ class Rough_Set_Transformer(nn.Module):
         self.bn1 = nn.BatchNorm1d(channels)
         self.bn2 = nn.BatchNorm1d(channels)
 
-        self.ra1 = RoughsetAttention(args, channels)
-        self.ra2 = RoughsetAttention(args, channels)
-        self.ra3 = RoughsetAttention(args, channels)
-        self.ra4 = RoughsetAttention(args, channels)
+        self.ra1 = GranularAttention(args, channels)
+        self.ra2 = GranularAttention(args, channels)
+        self.ra3 = GranularAttention(args, channels)
+        self.ra4 = GranularAttention(args, channels)
 
     def forward(self, x):
 
@@ -153,9 +153,9 @@ def Granulation(q, k):
     return matA_square + matB_square - 2. * (q @ k)
 
 
-class RoughsetAttention(nn.Module):
+class GranularAttention(nn.Module):
     def __init__(self, args, channels):
-        super(RoughsetAttention, self).__init__()
+        super(GranularAttention, self).__init__()
         self.args = args
         self.q_conv = nn.Conv1d(channels, channels // 4, 1, bias=False)
         self.k_conv = nn.Conv1d(channels, channels // 4, 1, bias=False)
@@ -183,39 +183,19 @@ class RoughsetAttention(nn.Module):
         x_q = self.v_conv(x)  # .permute(0, 2, 1)
         x_k = self.v_conv(x)
         x_v = self.v_conv(x).reshape(batch_size, q_len, -1, self.head_dim).transpose(1, 2)
-        # q = x.reshape(batch_size, -1, q_len, self.head_dim)  # F.softmax(x, dim=-1)  # self.act(self.after_norm(x))
         x_d = x.reshape(batch_size, -1, self.head_dim, q_len).reshape(batch_size*8, self.head_dim, -1, 16)
-        # q = self.deform_norm(x_d).flatten(2).transpose(-1, -2).reshape(batch_size, -1, q_len, self.head_dim)
         q = x_q.reshape(batch_size, -1, 8, self.head_dim).transpose(1, 2)
-        # q = x.transpose(-1, -2).reshape(batch_size, input_channels, -1, 16)
         N, hide_dim, height, weight = x_d.size()
-
-        # batch_size,  q_len, input_channels = x_q.size()
-        # batch_size,  input_channels, k_len = x_k.size()
-        # output = x_q.new(batch_size, 1, q_len, k_len)
 
         offsets = self.offsets(x_d)
         q_deformconv = self.deformconv(x_d, offsets)
         q_deformconv = self.deform_norm(q_deformconv)
-        # q_deformconv = q_deformconv.flatten(2).reshape(batch_size, int(N/batch_size), hide_dim, -1)
         q_deformconv = q_deformconv.flatten(2).reshape(batch_size, int(N/batch_size), hide_dim, -1).transpose(-1, -2).contiguous()
-        # q_deformconv = self.deform_norm(q_deformconv)
-        # q_deformconv = x_q.reshape(batch_size, -1, 8, self.head_dim).transpose(1, 2)
-        # q_deformconv = self.conv_norm(q_deformconv)
-        # k_deformconv = x_k.reshape(batch_size, -1, 8, self.head_dim).transpose(1, 2)
+
         x_r = _prob_QK(q, q_deformconv, x_v, self.args.max_iter)
-        # x_r = self.softmax(x_r)
-        # x_r = deform_attention(q, self.self_attn)
 
         # 计算注意力权重
-        # attention_weights = torch.sum(q_deformconv + k_deformconv, dim=-2)
         x_r = x_r / torch.sqrt(torch.tensor(self.head_dim).float())
-
-        # 对注意力权重进行softmax归一化
-        # attention_weights = self.softmax(attention_weights)
-
-        # 加权求和得到注意力输出
-        # x_r = (attention_weights.unsqueeze(-2) * x_v).transpose(-2, -3).flatten(2).contiguous()  # torch.sum(attention_weights.unsqueeze(-1) * x_v, dim=-2)
 
         x_r = self.act(self.after_norm(self.trans_conv(x - x_r)))
         x = x + x_r
@@ -227,11 +207,8 @@ def _prob_QK(query, q_deformconv, v, max_iter):  # (Q, K, v, sample_k, n_top):
     k_deformconv = q_deformconv
     kernel_1 = calMat(query, k_deformconv.transpose(-1, -2).contiguous())
     kernel_1 = torch.exp(-kernel_1 / 3.6)  # 1 / (1 + torch.exp(-0.2 * kernel_1))  # torch.sqrt(kernel_1 + 0.1**2)  # torch.sqrt(kernel_1)
-    # kernel_1 = F.softmax(kernel_1, dim=-1)
     kernel_2 = calMat(q_deformconv, k_deformconv.transpose(-1, -2).contiguous())
     kernel_2 = torch.exp(-kernel_2 / 3.6)  # 1 / (1 + torch.exp(-0.2 * kernel_2))  # torch.sqrt(kernel_2 + 0.1**2)  # torch.sqrt(kernel_2)
-    # kernel_2 = F.softmax(kernel_2, dim=-1)
-    # kernel_1 = F.dropout(kernel_1, p=dropout_p, training=training)8
     kernel_3 = kernel_1.transpose(-1, -2)
 
     Q_K = torch.matmul(torch.matmul(kernel_1, kerton_inv(kernel_2, max_iter)), torch.matmul(kernel_3, v))
@@ -249,43 +226,6 @@ def get_valid_ratio(mask):
     return valid_ratio
 
 
-def get_reference_points(spatial_shapes, valid_ratios, device):
-    reference_points_list = []
-    for lvl, (H_, W_) in enumerate(spatial_shapes):
-
-        ref_y, ref_x = torch.meshgrid(torch.linspace(0.5, H_ - 0.5, H_, dtype=torch.float32, device=device),
-                                      torch.linspace(0.5, W_ - 0.5, W_, dtype=torch.float32, device=device))
-        ref_y = ref_y.reshape(-1)[None] / (valid_ratios[:, None, lvl, 1] * H_)
-        ref_x = ref_x.reshape(-1)[None] / (valid_ratios[:, None, lvl, 0] * W_)
-        ref = torch.stack((ref_x, ref_y), -1)
-        reference_points_list.append(ref)
-    reference_points = torch.cat(reference_points_list, 1)
-    reference_points = reference_points[:, :, None] * valid_ratios[:, None]
-    return reference_points
-
-
-def deform_attention(query, attn):  # (Q, K, v, sample_k, n_top):
-    mask_flatten = []
-    spatial_shapes = []
-
-    bs, c, h, w = query.shape
-    mask = torch.full((bs, h, w), False, device=query.device)
-    spatial_shape = (h, w)
-    spatial_shapes.append(spatial_shape)
-    src_flatten = query.flatten(2).transpose(-1, -2)
-
-    mask_flatten = mask.flatten(1)
-    spatial_shapes = torch.as_tensor(spatial_shapes, dtype=torch.long, device=src_flatten.device)
-    level_start_index = torch.cat((spatial_shapes.new_zeros((1,)), spatial_shapes.prod(1).cumsum(0)[:-1]))
-    valid_ratios = torch.stack([get_valid_ratio(mask)], 1)
-
-    # 生成参考点坐标
-    reference_points = get_reference_points(spatial_shapes, valid_ratios, device=src_flatten.device)
-    attn_value = attn(src_flatten, reference_points, src_flatten, spatial_shapes, level_start_index, mask_flatten)
-
-    return attn_value  # , index_sample  QK_top Q_K.transpose(1, 2).flatten(2)
-
-
 def kernel_function(q, k):
     matA_square = q @ torch.ones(k.shape[-2:]).cuda()
     matB_square = torch.ones(q.shape[-2:]).cuda() @ k
@@ -301,11 +241,17 @@ def calMat(x, y):
     return c
 
 
-def kerton_inv(kernel2, max_iter=12):
-    # bs, n_head, m_len, nd = kernel2.shape
-    # matNum = kernel2.reshape(bs * n_head, m_len * nd).cpu().detach().numpy()
+def metricFrob(I, X, A):
+    N_X = torch.matmul(torch.matmul(A, X), A) - A
+    FrobX = torch.max(LA.matrix_norm(N_X, ord=2))
+    FrobA = torch.max(LA.matrix_norm(A, ord=2))
+    metric = FrobX / FrobA
+    return metric, FrobX
 
-    Frob = LA.matrix_norm(kernel2, ord='fro')  # .cpu().detach().numpy())
+
+def kerton_inv(kernel2, max_iter=12):
+
+    Frob = LA.matrix_norm(kernel2, ord='fro') 
     I = torch.eye(kernel2.size(-1), device=kernel2.device)
     I = torch.from_numpy(np.broadcast_to(I[None, None, ...].cpu().detach().numpy(), shape=kernel2.shape)).to(device=kernel2.device)
     A = kernel2  # + 0.01 * I
@@ -323,90 +269,6 @@ def kerton_inv(kernel2, max_iter=12):
         init_matNum = torch.max(LA.matrix_norm(initX, ord=2))
         err_cnt += 1
 
-    # Frob = np.linalg.norm(matNum, ord='fro')
-    # I = torch.eye(kernel2.size(-1), device=kernel2.device)
-    # A = kernel2 + 0.01 * I
-    # X = torch.from_numpy(np.broadcast_to(np.float32(2 / Frob)[..., None, None], shape=kernel2.shape)).to(device=kernel2.device)
-
-    metric, init0 = metricFrob(I, X, A)
-    # print("A0: " + str(metric.item()) + " init0: " + str(init0))
-    for i in range(20):
+    for i in range(max_iter):
         X = X @ (3 * I - 3 * A @ X + (A @ X) ** 2)
-        # metric, FrobX = metricFrob(I, X, A)
-        # print("A" + str(i + 1) + " : " + str(metric.item()) + " FrobX: " + str(FrobX))
     return X
-
-
-def metricFrob(I, X, A):
-    N_X = torch.matmul(torch.matmul(A, X), A) - A
-    FrobX = torch.max(LA.matrix_norm(N_X, ord=2))
-    FrobA = torch.max(LA.matrix_norm(A, ord=2))
-    metric = FrobX / FrobA
-    return metric, FrobX
-
-
-def newton_inv(mat, iter=6):
-    P = mat
-    I = torch.eye(mat.size(-1), device=mat.device)
-    I = torch.from_numpy(np.broadcast_to(I[None, None, ...].cpu().detach().numpy(), shape=P.shape)).to(device=P.device)
-    alpha = 2 / (torch.max(torch.sum(mat, dim=-1)) ** 2)
-    # alpha = 0.9 * 2 / torch.max(torch.sum(mat, dim=-1), -1)[0] ** 2
-    # P = P + 0.01 * I
-    V = alpha * P   # [..., None, None]
-    V2 = torch.from_numpy(np.broadcast_to(alpha[None, None, ...].cpu().detach().numpy(), shape=P.shape)).to(device=mat.device) * P
-
-    beta = 0.2
-    pnorm = torch.max(torch.sum(torch.abs(I - V), dim=-2))
-    err_cnt = 0
-    while pnorm > 0.9999 and err_cnt < 600:
-        alpha *= beta
-        V = torch.from_numpy(np.broadcast_to(alpha[None, None, ...].cpu().detach().numpy(), shape=P.shape)).to(device=mat.device) * P  # alpha * P
-        pnorm = torch.max(torch.sum(torch.abs(I - V), dim=-2))
-        err_cnt += 1
-
-    for i in range(iter):
-        V = 2 * V - V @ P @ V
-    return V
-
-
-def find_matroid_basis(matrix):
-    # 将矩阵转换为简化行阶梯形矩阵
-    row_echelon_matrix = np.array(matrix.cpu(), dtype=float)
-    m, n = row_echelon_matrix.shape
-    for i in range(min(m, n)):
-        # 找到第i列中第i行以下的最大值
-        max_element = abs(row_echelon_matrix[i, i])
-        max_row = i
-        for j in range(i+1, m):
-            if abs(row_echelon_matrix[j, i]) > max_element:
-                max_element = abs(row_echelon_matrix[j, i])
-                max_row = j
-        # 如果该列全为0，则继续下一列
-        if max_element == 0:
-            continue
-        # 将具有最大值的行与第i行交换
-        row_echelon_matrix[[i, max_row]] = row_echelon_matrix[[max_row, i]]
-        # 将主元归一化为1
-        row_echelon_matrix[i] /= row_echelon_matrix[i, i]
-        # 将主元所在列的其他元素消为0
-        for j in range(m):
-            if j != i:
-                row_echelon_matrix[j] -= row_echelon_matrix[i] * row_echelon_matrix[j, i]
-    # 找到主元所在的列，即拟阵基中向量的索引
-    basis_indices = np.where(np.abs(row_echelon_matrix).sum(axis=0) == 1)[0]
-    # 返回拟阵基中的向量
-    return matrix[:, basis_indices]
-
-
-if __name__ == '__main__':
-    # 测试代码
-    # matrix = np.array([[1, 2, 3, 4, 5],
-    #                   [0, 1, 2, 3, 4],
-    #                   [0, 0, 1, 2, 3],
-    #                   [2, 4, 6, 8, 10],
-    #                   [1, 0, 0, 0, 1]])
-    matrix = np.random.randint(0, 10, (150, 100))
-    basis = find_matroid_basis(matrix)
-    print(basis)
-    print(matrix)
-    print(basis.shape)
